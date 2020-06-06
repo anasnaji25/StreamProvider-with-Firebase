b2dbd18106db5b5ef2c221ae6ab298a4d56942a6
Atri Das <babinsamrat123@gmail.com>
2020-06-06 09:54:45 +0530

    creates the Login User Screen

--- lib/main.dart
+++ lib/main.dart
@@ -79,9 +79,60 @@ class _RegisterUserScreenState extends State<RegisterUserScreen> {
   }
 }
 
-class LoginUserScreen extends StatelessWidget {
+class LoginUserScreen extends StatefulWidget {
+  @override
+  _LoginUserScreenState createState() => _LoginUserScreenState();
+}
+
+class _LoginUserScreenState extends State<LoginUserScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String error = '';
+
+  // text field state
+  String email = '';
+  String password = '';
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.indigo,
+        title: Text('SignIn User'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(hintText: 'email'),
+                validator: (val) => val.isEmpty ? 'Enter an email' : null,
+                onChanged: (val) {
+                  setState(() => email = val);
+                },
+              ),
+              SizedBox(height: 20.0),
+              TextFormField(
+                decoration: InputDecoration(hintText: 'password'),
+                obscureText: true,
+                validator: (val) =>
+                val.length < 6 ? 'Enter a password 6+ chars long' : null,
+                onChanged: (val) {
+                  setState(() => password = val);
+                },
+              ),
+              /*Register button*/
+              RaisedButton(
+                color: Colors.redAccent,
+                onPressed: () {},
+                child: Text('Sign In'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
+
