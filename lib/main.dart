64a10f15b8ae4b418f2a129ec17e14aada0f38cc
Atri Das <babinsamrat123@gmail.com>
2020-06-06 09:32:57 +0530

    cleans some unusuals

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutterstreamprovider/logic/model/user_model.dart';
-import 'package:flutterstreamprovider/logic/services/firebase_services.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,59 +7,23 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final FirebaseServices firebaseServices = FirebaseServices();
-
     return MaterialApp(
-      home: StreamProvider(
-        create: (BuildContext context) => firebaseServices.getUserList(),
-        child: ViewUserPage(),
-      ),
+      home: RegisterUserScreen(),
     );
   }
 }
 
-class ViewUserPage extends StatelessWidget {
+
+class RegisterUserScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return Container();
+  }
+}
 
-    List userList = Provider.of<List<UserModel>>(context);
-    FirebaseServices firebaseServices = FirebaseServices();
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.amber,
-        title: Text('Stream Provider'),
-      ),
-      body: userList == null ? CircularProgressIndicator() :  ListView.builder(
-        itemCount: userList.length,
-        itemBuilder: (_, int index) => Padding(
-          padding: EdgeInsets.all(10.0),
-          child: ListTile(
-            leading: CircleAvatar(
-              backgroundColor: Colors.amber,
-            ),
-            title: Text(
-              userList[index].name,
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 20.0,
-              ),
-            ),
-            subtitle: Text(
-              userList[index].age,
-              style: TextStyle(
-                fontSize: 25.0,
-              ),
-            ),
-          ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          firebaseServices.addUser();
-        },
-        backgroundColor: Colors.amber,
-      ),
-    );
+class LoginUserScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
